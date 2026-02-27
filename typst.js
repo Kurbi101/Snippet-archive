4e4f617e44f8cd18c75ffcc212b5b3eedf5cdea9
omer <sheygetz@gmail.com>
2026-02-27 22:54:08 +0200

    initial

---
+++ typst.js
@@ -0,0 +1,38 @@
+[
+  // Math Mode
+  { trigger: "Mk", replacement: "$$0$", options: "tA" },
+  { trigger: "Dm", replacement: "$$\n$0\n$$", options: "tAw" },
+  { trigger: "mk", replacement: "$$0$", options: "tA" },
+  { trigger: "dm", replacement: "$$\n$0\n$$", options: "tAw" },
+
+  //Greek letters
+  { trigger: "@a", replacement: "alpha", options: "mA" },
+  { trigger: "@b", replacement: "beta", options: "mA" },
+  { trigger: "@g", replacement: "gamma", options: "mA" },
+  { trigger: "@G", replacement: "Gamma", options: "mA" },
+  { trigger: "@d", replacement: "delta", options: "mA" },
+  { trigger: "@D", replacement: "Delta", options: "mA" },
+  { trigger: "@e", replacement: "epsilon", options: "mA" },
+  { trigger: ":e", replacement: "varepsilon", options: "mA" },
+  { trigger: "@z", replacement: "zeta", options: "mA" },
+  { trigger: "@t", replacement: "theta", options: "mA" },
+  { trigger: "@T", replacement: "Theta", options: "mA" },
+  { trigger: ":t", replacement: "vartheta", options: "mA" },
+  { trigger: "@i", replacement: "iota", options: "mA" },
+  { trigger: "@k", replacement: "kappa", options: "mA" },
+  { trigger: "@l", replacement: "lambda", options: "mA" },
+  { trigger: "@L", replacement: "Lambda", options: "mA" },
+  { trigger: "@s", replacement: "sigma", options: "mA" },
+  { trigger: "@S", replacement: "Sigma", options: "mA" },
+  { trigger: "@u", replacement: "upsilon", options: "mA" },
+  { trigger: "@U", replacement: "Upsilon", options: "mA" },
+  { trigger: "@o", replacement: "omega", options: "mA" },
+  { trigger: "@O", replacement: "Omega", options: "mA" },
+  { trigger: "@r", replacement: "rho", options: "mA" },
+  { trigger: "@R", replacement: "Rho", options: "mA" },
+  { trigger: "ome", replacement: "omega", options: "mA" },
+  { trigger: "Ome", replacement: "Omega", options: "mA" },
+
+  //Expanded
+  { trigger: "sum", replacement: "sum_($0{i = 1})^($1{n}) $2" },
+];
